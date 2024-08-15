eb3625a062879d6871ff241f62edb2a59d469ca1
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-08-15 18:21:48 -0300

    titulo

---
+++ src/components/Titulo/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Titulo.module.css"
+
+const Titulo = ({ children }) => {
+    return (
+        <h1>{children}</h1>
+    )
+}
+
+export default Titulo
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import Banner from "components/Banner";
 import Footer from "components/Footer";
 import Header from "components/Header";
+import Titulo from "components/Titulo";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Banner imagem='home'/>
+      <Titulo>Um lugar para guardar seus vídeos e filmes!</Titulo>
       <Footer />
     </>
   );
