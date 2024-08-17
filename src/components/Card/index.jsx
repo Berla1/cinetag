62524ef4ea2ba1bd8cb66ba8dc9642fa6341be2c
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-08-17 20:19:51 -0300

    pagina de player dinamica

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,22 +2,25 @@ import styles from "./Card.module.css";
 import iconeFavoritar from "./favoritar.png";
 import iconeCoracaoFavorito from "./desfavoritar.png";
 import { useFavoritoContext } from "components/context/favoritos";
+import { Link } from "react-router-dom";
 
 const Card = ({ id, titulo, capa }) => {
-  const {favorito, addFavorito} = useFavoritoContext();
+  const { favorito, addFavorito } = useFavoritoContext();
   const ehFavorito = favorito.some((fav) => fav.id === id);
-  const icone = ehFavorito ? iconeCoracaoFavorito : iconeFavoritar
+  const icone = ehFavorito ? iconeCoracaoFavorito : iconeFavoritar;
   return (
     <>
       <div className={styles.container}>
-        <img src={capa} alt={titulo} />
-        <h2>{titulo}</h2>
+        <Link className={styles.card} to={`/${id}`}>
+          <img src={capa} alt={titulo}/>
+          <h2>{titulo}</h2>
+        </Link>
         <img
           className={styles.favoritar}
           src={icone}
           alt="Favoritar filme"
           onClick={() => {
-            addFavorito({id, titulo, capa})
+            addFavorito({ id, titulo, capa });
           }}
         />
       </div>
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Footer from "components/Footer";
 import Header from "components/Header";
 import Favoritos from "pages/Favoritos";
 import Home from "pages/Home";
+import Player from "pages/Player";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function AppRoutes() {
@@ -15,6 +16,7 @@ function AppRoutes() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/favoritos" element={<Favoritos />} />
+            <Route path="/:id" element={<Player/>} />
           </Routes>
         </FavoritosProvider>
         <Footer />
