f693ad2b1f7946fd8cc21a2ea10cdf4d6bfb86ad
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2024-08-18 14:56:06 -0300

    finalizando projeto

--- src/pages/Player/index.jsx
+++ src/pages/Player/index.jsx
@@ -2,18 +2,26 @@ import Banner from "components/Banner";
 import styles from "./Player.module.css";
 import Titulo from "components/Titulo";
 import { useParams } from "react-router-dom";
-import videos from "json/db.json";
 import NaoEncontrada from "pages/NaoEncontrada";
+import { useEffect, useState } from "react";
 
 const Player = () => {
+  const [video, setVideo] = useState();
   const parametros = useParams();
-  const video = videos.find((video) => {
-    return video.id === Number(parametros.id);
-  });
-  
+
+  useEffect(() => {
+    fetch(
+      `https://my-json-server.typicode.com/Berla1/cinetag-api/videos?id=${parametros.id}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setVideo(...data);
+      });
+  }, []);
+
   if (!video) {
-    return <NaoEncontrada /> 
-}
+    return <NaoEncontrada />;
+  }
 
   return (
     <>
@@ -21,12 +29,14 @@ const Player = () => {
       <Titulo>Player</Titulo>
       <section className={styles.video}>
         <iframe
-            width="70%"
-            height="100%"
-            src={video.link}
-            title={video.titulo}
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          width="70%"
+          height="100%"
+          src={video.link}
+          title={video.titulo}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </section>
     </>
   );
